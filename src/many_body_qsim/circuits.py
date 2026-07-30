db3c6bc1f8ee41cafe0b6c2af590f2773e5aa09e
Brendan Stork <brendanstork@gmail.com>
2026-07-30 14:28:55 -0700

    Draw and layers methods for QuantumCircuit

--- src/many_body_qsim/circuits.py
+++ src/many_body_qsim/circuits.py
@@ -28,6 +28,7 @@ class Quantum_Circuit:
         self.numqubits = numqubits
         self.state = np.zeros(2**numqubits, dtype=complex)
         self.state[0] = 1
+        self.gates = []
         self.gate_count = 0
 
     def gate_op(self, gate, target):
@@ -56,31 +57,38 @@ class Quantum_Circuit:
 
     def x(self, qubitIndex):
         self.gate_op(GATES['X'], qubitIndex)
+        self.gates.append(('X', qubitIndex))
         return
 
         
     def y(self, qubitIndex):
         self.gate_op(GATES['Y'], qubitIndex)
+        self.gates.append(('Z', qubitIndex))
         return
         
     def z(self, qubitIndex):
         self.gate_op(GATES['Z'], qubitIndex)
+        self.gates.append(('Z', qubitIndex))
         return
         
     def h(self, qubitIndex):
         self.gate_op(GATES['H'], qubitIndex)
+        self.gates.append(('H', qubitIndex))
         return
 
     def s(self, qubitIndex):
         self.gate_op(GATES['S'], qubitIndex)
+        self.gates.append(('S', qubitIndex))
+        return
+        
+    def sdag(self, qubitIndex):
+        self.gate_op(GATES['Sdag'], qubitIndex)
+        self.gates.append(('S\u2020', qubitIndex))
         return
 
     def t(self, qubitIndex):
         self.gate_op(GATES['T'], qubitIndex)
-        return
-
-    def sdag(self, qubitIndex):
-        self.gate_op(GATES['Sdag'], qubitIndex)
+        self.gates.append(('T', qubitIndex))
         return
 
     def p(self, qubitIndex, theta):
@@ -89,32 +97,25 @@ class Quantum_Circuit:
 
     def rx(self, qubitIndex, theta):
         self.gate_op(GATES['RX'](theta), qubitIndex)
+        self.gates.append(('RX', qubitIndex, theta))
         return
 
     def ry(self, qubitIndex, theta):
         self.gate_op(GATES['RY'](theta), qubitIndex)
+        self.gates.append(('RY', qubitIndex, theta))
         return
         
     def rz(self, qubitIndex, theta):
         self.gate_op(GATES['RZ'](theta), qubitIndex)
+        self.gates.append(('RZ', qubitIndex, theta))
         return
 
     def cx(self, control, target):
         self.state = apply_cnot(self.state, control, target)
+        self.gates.append(('CX', control, target))
         return
         
-    '''
-    def expectation_value(self, pauli_string):
 
-        if len(pauli_string) != self.numqubits:
-            raise ValueError('Pauli string length must equal number of qubits')
-
-        operator = GATES[pauli_string[0]] # Builds operator
-        for p in pauli_string[1:]:
-            operator = np.kron(operator, GATES[p])
-        return np.vdot(self.state, operator @ self.state).real # Expectation value
-    
-    '''
     def expectation_value(self, pauli_string):
 
         if len(pauli_string) != self.numqubits:
@@ -158,3 +159,103 @@ class Quantum_Circuit:
         new_qc.state = self.state.copy()
 
         return new_qc
+        
+    def layers(self):
+
+        layers = []
+
+        for gate in self.gates:
+
+            if gate[0] == 'CX':
+                used = {gate[1], gate[2]}
+                
+            else:
+                used = {gate[1]}
+
+            placed = False
+
+            for layer in layers:
+
+                occupied = set()
+
+                for g in layer:
+
+                    if g[0] == 'CX':
+                        occupied.update([g[1], g[2]])
+                        #occupied.update([range(g[1], g[2])])
+                        
+                    else:
+                        occupied.add(g[1])
+
+                if occupied.isdisjoint(used):
+                    layer.append(gate)
+                    placed = True
+                    break
+
+            if not placed:
+                layers.append([gate])
+
+        return layers
+    
+    def draw(self):
+
+        WIDTH = 8
+        circuit_line = '─' * WIDTH
+        lines = [
+            f'q{i}: {'─' * 2}'
+            for i in range(self.numqubits)
+        ]
+
+        for layer in self.layers():
+
+            # Start every qubit with a horizontal wire
+            cells = ['─' * (WIDTH + 1) for _ in range(self.numqubits)]
+
+            for gate in layer:
+
+                name = gate[0]
+
+                # -------------------------
+                # Single-qubit gates
+                # -------------------------
+
+                if name != 'CX':
+
+                    target = gate[1]
+
+                    if name.startswith('R'):
+                        label = name #f'{name}{circuit_line}' #({gate[2]:.2f})'
+                        cells[target] = f'{label}{circuit_line[:-1]}'
+                    else:
+                        label = name
+                        cells[target] = f'{label}{circuit_line}'
+                    #cells[target] = f'{label}{circuit_line}'
+
+
+                # -------------------------
+                # CNOT
+                # -------------------------
+
+                else:
+
+                    control = gate[1]
+                    target = gate[2]
+
+                    low = min(control, target)
+                    high = max(control, target)
+
+                    cells[control] = f'{"●"}{circuit_line}'
+                    cells[target] = f'{"X"}{circuit_line}'
+
+                    #for q in range(low, high):
+                    #    cells[q] = f'{"│"}{circuit_line}'
+
+
+            # Append this layer to the circuit
+            for q in range(self.numqubits):
+                lines[q] += cells[q]
+
+        for line in lines:
+            print(line)
+        
+    
